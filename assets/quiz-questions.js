4d79a7b82a3cce98233d0aafe8d2490b892e65ff
pguerra98 <119546662+pguerra98@users.noreply.github.com>
2023-01-24 19:54:52 -0500

    This is an update push, made some progress

---
+++ assets/quiz-questions.js
@@ -0,0 +1,63 @@
+var questions = [
+    {
+        question: "_____ is the process of finding and fixing errors within a given program.",
+        choices: ["Scanning", "Executing", "Compiling", "Debugging"],
+        answer: "Debugging"
+    },
+
+    {
+        question: "In Java, which of the followning is an array",
+        choices: ["Primitive data type", "Object References", "Objects", "None of the above"],
+        answer: "Objects"
+    },
+
+    {
+        question: "What is the correct definition of a package?",
+        choices: ["A package is a collection of classes and interfaces", "A package is a collectin of classes", 
+                  "A package is a collection of editing tools", "A package is a collection of interfaces"],
+        answer: "A package is a collection of classes and interfaces"
+    },
+
+    {
+        question: "Which is the correct way to declare core methods in Java?",
+        choices: ["Comparator", "Set", "Collection", "EventListener"],
+        answer: "Collection"
+    },
+
+    {
+        question: "What does the abbreviation HTML stand for?",
+        choices: ["HighText Markup Language", "HyperText Markup Language", "HyperText Markdown Language", "None of the above"],
+        answer: "HyperText Markup Language"
+    },
+
+    {
+        question: "Which of the following is the smallest header in HTML by default?",
+        choices: ["h2", "h1", "h4", "h6"],
+        answer: "h6"
+    },
+
+    {
+        question: "Which of the following does not require a closing tag?",
+        choices: ["<br>", "<hr>", "Both A and B", "None of the above"],
+        answer: "Both A and B"
+    },
+
+    {
+        question: "Which of the following attributes is utilized to provide a unique name to an HTML element?",
+        choices: ["type", "class", "id", "None of the above"],
+        answer: "id"
+    },
+
+    {
+        question: "Which of the following tags is utilized to render an image on a webpage?",
+        choices: ["image", "img", "src", "None of the above"],
+        answer: "img"
+    },
+
+    {
+        question: "Which of the following tags is utilized to show the page's start and endpoints?",
+        choices: ["<body>", "<doctype>", "<head>", "<html>"],
+        answer: "<html>"
+    }
+
+];
